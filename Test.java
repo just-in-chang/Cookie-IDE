02e2d1ee1be5e6952ec8a091c60d8bde40aa4a05
jchang188 <jchang188@lhs.win.fuhsd.org>
2019-04-29 09:13:51 -0700

    first kiss uwu

---
+++ Test.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Test extends Application
+{
+    public static void main( String[] args )
+    {
+
+    }
+
+
+    @Override
+    public void start( Stage arg0 )
+    {
+
+    }
+
+}
