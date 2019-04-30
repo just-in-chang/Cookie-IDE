131614e6c0a4738cc5ebf5f455a027fdb5c536fa
Andrew <andrewchen118@gmail.com>
2019-04-30 08:29:39 -0700

    bleh

--- Test.java
+++ Test.java
@@ -1,19 +1,31 @@
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
 public class Test extends Application
 {
+    /**
+     * main class lmao
+     * @param args command line arguments USED!
+     */
     public static void main( String[] args )
     {
-
+        launch(args);
     }
 
 
     @Override
     public void start( Stage arg0 )
     {
-
+        arg0.setTitle( "lmao" );
+        Button button = new Button("ree");
+        
+        Pane layout = new Pane();
+        layout.getChildren().add( button );
+        
+        
     }
 
 }
