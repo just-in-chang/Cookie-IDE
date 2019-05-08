46107ef8073ff8a7c5b4d2a40b1947661fdcd66f
Andrew <andrewchen118@gmail.com>
2019-05-07 23:35:52 -0700

    mASSive progress in node adding mechanics

--- controlButton.java
+++ controlButton.java
@@ -1,20 +1,32 @@
+import java.util.Stack;
+
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.layout.*;
 
+
 public class controlButton extends Button
 {
-//    public controlButton()
-//    {
-//        super();
-//    }
-    int count = 1;
     
-    public controlButton( String text, Pane workspace, Control control, int x, int y )
+    private int count = 1;
+
+    public controlButton( String text, Pane workspace, Class<?> c, Stack<Node> s )
     {
-        super(text);
+        super( text );
         this.setOnMouseClicked( e -> {
-            workspace.getChildren().add(  );
-        });
+            try
+            {
+                Control node = (Control)c.getConstructor( String.class )
+                    .newInstance( text + count );
+                workspace.getChildren().add( node );
+                count++;
+                s.push( node );
+            }
+            catch ( Exception ex )
+            {
+                System.out.println( ex );
+            }
+        } );
     }
 }
