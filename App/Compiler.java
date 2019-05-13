e8b01d180e3a9372353326abb986052cc0ec3187
Andrew Chen <andrewchen118@gmail.com>
2019-05-13 10:42:43 -0700

    make threads daemon

--- App/Compiler.java
+++ App/Compiler.java
@@ -95,6 +95,9 @@ public class Compiler
                     }
                 }
             };
+            
+            putOut.setDaemon( true );
+            takeIn.setDaemon( true );
 
             putOut.start();
             takeIn.start();
